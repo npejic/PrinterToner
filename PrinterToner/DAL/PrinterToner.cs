0417da72457374ff7ac7acaf3a914b8a941dabc9
nik <npejic@gmail.com>
2017-02-03 22:44:58 +0100

    Pocetak - ASP.Net, dodat PrinterTonerContext

---
+++ PrinterToner/DAL/PrinterToner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PrinterTonerEPC.Models;
+using System.Data.Entity;
+
+namespace PrinterTonerEPC.DAL
+{
+    public class PrinterTonerContext : DbContext
+    {
+        public PrinterTonerContext() : base("PrinterTonerContext") { }
+
+         public DbSet<Owner> Owners { get; set; }
+
+         public System.Data.Entity.DbSet<PrinterTonerEPC.Models.Printer> Printers { get; set; }
+    }
+}
