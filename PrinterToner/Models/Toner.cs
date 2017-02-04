530eae4a0455a9dde8aa6e0f025fed091480e0e3
nik <npejic@gmail.com>
2017-02-04 12:22:28 +0100

    Toner Class created

---
+++ PrinterToner/Models/Toner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrinterToner.Models
+{
+    public class Toner
+    {
+        public int TonerID { get; set; }
+        [Required(ErrorMessage = "Morate uneti model tonera")]
+        public int TonerModel { get; set; }
+        public bool TonerIsOriginal { get; set; }
+    }
+}
